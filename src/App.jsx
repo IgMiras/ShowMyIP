eefb1a0b5af1792e231ab1193e480c553bea07c7
IgMiras <im.miras@unesp.br>
2024-01-19 20:55:07 -0300

    funciona amigao

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import IPInfos from "./components/IPInfos"
 import '@tomtom-international/web-sdk-maps/dist/maps.css'
 import tt from '@tomtom-international/web-sdk-maps'
 import { useState, useRef, useEffect } from "react"
+import Axios from 'axios'
 
 
 const App = () => {
@@ -11,8 +12,25 @@ const App = () => {
   const [mapLatitude, setMapLatitude] = useState(0)
   const [mapZoom, setMapZoom] = useState(13)
   const [map, setMap] = useState({})
+  const [ipInfos, setIpInfos] = useState({})
 
   useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await Axios.get('http://localhost:3001/')
+        console.log('Vindo do localhost: ', response)
+        const userIP = response.data.ip
+        const infoResponse = await Axios.get(`https://ipapi.co/${userIP}/json`)
+        console.log('Vindo de ipapi: ', infoResponse)
+        setIpInfos(infoResponse.data)
+        setMapLongitude(infoResponse.data.longitude)
+        setMapLatitude(infoResponse.data.latitude)
+      } catch (err) {
+        console.error('Error while GET Api: ', err)
+      }
+    }
+    fetchData()
+
     let map = tt.map({
       key: 'itH5JL3iSUKfcpYTMX1VSSMjaxduVfmV',
       container: mapElement.current,
@@ -22,17 +40,14 @@ const App = () => {
     console.log(mapLongitude, mapLatitude)
     setMap(map)
     return () => map.remove()
-  }, [mapLatitude, mapLongitude])
+  }, [])
 
   return (
     <>
       <Navbar />
       <div className="flex justify-center h-screen max-w-[1240px] mx-auto">
         <div className='flex justify-around items-center flex-col bg-slate-900 h-full w-full border-solid border-2 border-[#22c55e] text-light-gray'>
-          <IPInfos 
-            setMapLongitude={setMapLongitude}
-            setMapLatitude={setMapLatitude}  
-          />
+          <IPInfos ipInfos={ipInfos}/>
           <div ref={mapElement} className="mapDiv h-4/6 w-10/12"></div>
         </div>
       </div>
--- src/components/IPInfos.jsx
+++ src/components/IPInfos.jsx
@@ -2,48 +2,35 @@ import { useState, useEffect } from "react"
 import Axios from 'axios'
 
 const IPInfos = (props) => {
-    const [ipInfos, setIpInfos] = useState({})
-
-    const fetchData = () => {
-        Axios.get('http://ip-api.com/json/').then((res) => {
-            setIpInfos(res.data)
-            props.setMapLongitude(res.data.lon)
-            props.setMapLatitude(res.data.lat)
-        }).catch((err) => console.log(err))
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
 
     return (
         <div>
           <div className="overflow-x-auto">
             <table className="min-w-full">
               <tbody>
-                  <tr key={ipInfos.query} className="border-b">
+                  <tr key={props.ipInfos.query} className="border-b">
                     <td className="text-left px-4 py-2 font-semibold">IP:</td>
-                    <td className="text-left px-4 py-2">{ipInfos.query}</td>
+                    <td className="text-left px-4 py-2">{props.ipInfos.query}</td>
                   </tr>
-                  <tr key={ipInfos.country} className="border-b">
+                  <tr key={props.ipInfos.country} className="border-b">
                     <td className="text-left px-4 py-2 font-semibold">Country:</td>
-                    <td className="text-left px-4 py-2">{ipInfos.country}</td>
+                    <td className="text-left px-4 py-2">{props.ipInfos.country}</td>
                   </tr>
-                  <tr key={ipInfos.regionName} className="border-b">
+                  <tr key={props.ipInfos.regionName} className="border-b">
                     <td className="text-left px-4 py-2 font-semibold">Region:</td>
-                    <td className="text-left px-4 py-2">{ipInfos.regionName}</td>
+                    <td className="text-left px-4 py-2">{props.ipInfos.regionName}</td>
                   </tr>
-                  <tr key={ipInfos.city} className="border-b">
+                  <tr key={props.ipInfos.city} className="border-b">
                     <td className="text-left px-4 py-2 font-semibold">City:</td>
-                    <td className="text-left px-4 py-2">{ipInfos.city}</td>
+                    <td className="text-left px-4 py-2">{props.ipInfos.city}</td>
                   </tr>
-                  <tr key={ipInfos.isp} className="border-b">
+                  <tr key={props.ipInfos.isp} className="border-b">
                     <td className="text-left px-4 py-2 font-semibold">ISP:</td>
-                    <td className="text-left px-4 py-2">{ipInfos.isp}</td>
+                    <td className="text-left px-4 py-2">{props.ipInfos.isp}</td>
                   </tr>
-                  <tr key={ipInfos.org} className="border-b">
+                  <tr key={props.ipInfos.org} className="border-b">
                     <td className="text-left px-4 py-2 font-semibold">Organization:</td>
-                    <td className="text-left px-4 py-2">{ipInfos.org}</td>
+                    <td className="text-left px-4 py-2">{props.ipInfos.org}</td>
                   </tr>
               </tbody>
             </table>
